267f3988b1418c8261999f7473b646663b53cc0f
bitlover2009 <bitlover2009@gmail.com>
2024-01-20 19:22:08 -0600

    Initial Commit

---
+++ Veterinaria/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Veterinaria
+//
+//  Created by David Grau Beltrán  on 20/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
